ab05e646306aa2a3ce07bd74db595c9d8199f1f5
Yuri <namyuri@gmail.com>
2022-12-30 19:44:22 +0900

    Webtoon App - add 2 API service functions

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,6 +1,8 @@
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
+import 'package:toonflix/models/webtoon_detail_model.dart';
+import 'package:toonflix/models/webtoon_episode_model.dart';
 import 'package:toonflix/models/webtoon_model.dart';
 
 class AppService {
@@ -19,6 +21,32 @@ class AppService {
         webtoonInstances.add(WebtoonModel.fromJson(webtoon));
       }
       return webtoonInstances;
+    }
+    throw Error();
+  }
+
+  static Future<WebtoonDetailModel> getToonById(String id) async {
+    final url = Uri.parse("$baseUrl/$id");
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      final webtoonDetail = jsonDecode(response.body);
+      return WebtoonDetailModel.fromJson(webtoonDetail);
+    }
+    throw Error();
+  }
+
+  static Future<List<WebtoonEpisodeModel>> getLatestEpisodesById(
+      String id) async {
+    List<WebtoonEpisodeModel> webtoonEpisodes = [];
+
+    final url = Uri.parse("$baseUrl/$id/episodes");
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      final List<dynamic> webtoonEpisodeList = jsonDecode(response.body);
+      for (var webtoon in webtoonEpisodeList) {
+        webtoonEpisodes.add(WebtoonEpisodeModel.fromJson(webtoon));
+      }
+      return webtoonEpisodes;
     }
     throw Error();
   }
