5f766bc9cffddcd92beda494ce6416aac9751ae2
Yuri <namyuri@gmail.com>
2022-12-30 15:59:43 +0900

    Webtoon App - AppBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:toonflix/screen/home_screen.dart';
+import 'package:toonflix/screens/home_screen.dart';
 
 void main() {
   runApp(const App());
@@ -10,16 +10,6 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        theme: ThemeData(
-          backgroundColor: const Color(0xFFE7626C),
-          textTheme: const TextTheme(
-            headline1: TextStyle(
-              color: Color(0xFF232B55),
-            ),
-          ),
-          cardColor: const Color(0xFFF4EDDB),
-        ),
-        home: const HomeScreen());
+    return const MaterialApp(home: HomeScreen());
   }
 }
