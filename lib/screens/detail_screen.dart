dd6bb32a35513ffff22253da1d16205583e7cb3a
Yuri <namyuri@gmail.com>
2022-12-31 19:04:39 +0900

    Webtoon App - Futures

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -1,13 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:toonflix/models/webtoon_detail_model.dart';
+import 'package:toonflix/models/webtoon_episode_model.dart';
+import 'package:toonflix/services/api_service.dart';
 
-class DetailScreen extends StatelessWidget {
+class DetailScreen extends StatefulWidget {
   final String title, thumb, id;
+
   const DetailScreen({
     super.key,
     required this.title,
     required this.thumb,
     required this.id,
   });
+
+  @override
+  State<DetailScreen> createState() => _DetailScreenState();
+}
+
+class _DetailScreenState extends State<DetailScreen> {
+  late Future<WebtoonDetailModel> webtoonDetail;
+  late Future<List<WebtoonEpisodeModel>> episode;
+
+  @override
+  void initState() {
+    super.initState();
+    webtoonDetail = AppService.getToonById(widget.id);
+    episode = AppService.getLatestEpisodesById(widget.id);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,7 +37,7 @@ class DetailScreen extends StatelessWidget {
         backgroundColor: Colors.white,
         foregroundColor: Colors.green,
         title: Text(
-          title,
+          widget.title,
           style: const TextStyle(
             fontSize: 24,
             fontWeight: FontWeight.w400,
@@ -34,7 +53,7 @@ class DetailScreen extends StatelessWidget {
                 height: 50,
               ),
               Hero(
-                tag: id,
+                tag: widget.id,
                 child: Container(
                   decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(10),
@@ -48,7 +67,7 @@ class DetailScreen extends StatelessWidget {
                   ),
                   clipBehavior: Clip.hardEdge,
                   width: 250,
-                  child: Image.network(thumb),
+                  child: Image.network(widget.thumb),
                 ),
               ),
             ],
